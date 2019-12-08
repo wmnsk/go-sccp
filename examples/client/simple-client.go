079cb730065eb8683d3a2f5e819b4e3000ec7676
Yoshiyuki Kurauchi <ahochauwaaaaa@gmail.com>
2019-12-08 20:21:22 +0900

    fix typo in example

--- examples/client/simple-client.go
+++ examples/client/simple-client.go
@@ -26,7 +26,7 @@ import (
 
 func main() {
 	var (
-		addr  = flag.String("aadr", "127.0.0.1:2905", "Remote IP and Port to connect to.")
+		addr  = flag.String("addr", "127.0.0.1:2905", "Remote IP and Port to connect to.")
 		data  = flag.String("data", "deadbeef", "Payload to send on UDT in hex format.")
 		hbInt = flag.Duration("hb-interval", 0, "Interval for M3UA BEAT. Put 0 to disable")
 	)
